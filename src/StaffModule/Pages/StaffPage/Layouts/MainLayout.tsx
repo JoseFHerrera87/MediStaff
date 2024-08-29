9c883ffbfa0d48a2a1f3d98c3b243cdcc75095b6
Francisco23 <joseherrera282002@gmail.com>
2024-08-28 22:36:11 -0600

    Se removio toda logica del layout main y se translada al staffPage, adicionalmente la logica se separa en hooks

--- src/StaffModule/Pages/StaffPage/Layouts/MainLayout.tsx
+++ src/StaffModule/Pages/StaffPage/Layouts/MainLayout.tsx
@@ -1,52 +1,14 @@
-import { StaffCard } from "../Components/StaffCard";
 import "../Styles/mainStyle.css";
-import { LoadLayout } from "./LoadLayout";
-
-const getEmployees = async () => {
-  try {
-    const res = await fetch("http://localhost:3000");
-
-    if (!res.ok) {
-      throw new Error("Error al hacer fetch");
-    }
-
-    const employees = await res.json();
-    console.log(employees);
-  } catch (error) {
-    console.error("Failed to fetch employees:", error);
-  }
-};
-
-getEmployees();
-export const MainLayout = () => {
-  const isLoading: boolean = false;
-  if (isLoading)
-    return (
-      <main className="main__main">
-        <LoadLayout />
-      </main>
-    );
-
+export const MainLayout: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
   return (
     <main className="main__main">
-      {isLoading && <LoadLayout />}
-
       <div className="main__container-cards">
         <header className="main__header">
           <h1 className="header__title">Todos</h1>
         </header>
-
-        {/* {employees.map((employee) => (
-          <StaffCard
-            id={employee.id}
-            firstName={employee.firstName}
-            lastName={employee.lastName}
-            department={employee.department}
-            email={employee.email}
-            img={employee.img}
-            key={employee.id}
-          />
-        ))} */}
+        {children}
       </div>
     </main>
   );
--- src/StaffModule/Pages/StaffPage/StaffPage.tsx
+++ src/StaffModule/Pages/StaffPage/StaffPage.tsx
@@ -1,16 +1,22 @@
 import { StaffLayout } from "./Layouts/HeaderLayout";
 import { AsideLayout } from "./Layouts/AsideLayout";
 import { MainLayout } from "./Layouts/MainLayout";
-
+import { StaffCard } from "./Components/StaffCard";
 import "./Styles/staffPage.css";
+import { useFetch } from "./hooks/useFetch";
 
 export const StaffPage = () => {
+  const employees = useFetch("http://localhost:3000/employees");
+
+  console.log(employees);
   return (
     <>
       <StaffLayout />
       <div className="principal">
         <AsideLayout />
-        <MainLayout />
+        <MainLayout>
+          <div>Vacio</div>
+        </MainLayout>
       </div>
     </>
   );
